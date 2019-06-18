2f210841a124e3b3f8a546077690b8f71e6f2cb8
Travis CI User <travis@example.org>
2019-06-18 16:04:27 +0000

    Update documentation

---
+++ implementors/core/iter/traits/iterator/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["vst"] = [{text:"impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"vst/buffer/struct.InputIterator.html\" title=\"struct vst::buffer::InputIterator\">InputIterator</a>&lt;'a, T&gt;",synthetic:false,types:["vst::buffer::InputIterator"]},{text:"impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"vst/buffer/struct.OutputIterator.html\" title=\"struct vst::buffer::OutputIterator\">OutputIterator</a>&lt;'a, T&gt;",synthetic:false,types:["vst::buffer::OutputIterator"]},{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"vst/api/struct.EventIterator.html\" title=\"struct vst::api::EventIterator\">EventIterator</a>&lt;'a&gt;",synthetic:false,types:["vst::api::EventIterator"]},{text:"impl&lt;'pt&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"vst/util/struct.ParameterTransferIterator.html\" title=\"struct vst::util::ParameterTransferIterator\">ParameterTransferIterator</a>&lt;'pt&gt;",synthetic:false,types:["vst::util::parameter_transfer::ParameterTransferIterator"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
